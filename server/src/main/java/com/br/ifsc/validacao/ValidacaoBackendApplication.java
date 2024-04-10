bfd6c1b8de6eca81c16a724f586f5440533b181b
Eynar Baron <eynartb@gmail.com>
2024-04-09 22:56:50 -0300

    Initial commit

---
+++ server/src/main/java/com/br/ifsc/validacao/ValidacaoBackendApplication.java
@@ -0,0 +1,13 @@
+package com.br.ifsc.validacao;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ValidacaoBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ValidacaoBackendApplication.class, args);
+	}
+
+}
---
+++ server/src/test/java/com/br/ifsc/validacao/ValidacaoBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.br.ifsc.validacao;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ValidacaoBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
